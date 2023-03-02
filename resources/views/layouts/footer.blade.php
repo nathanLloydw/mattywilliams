4ef3f9c0ffb10b41e3e71b3166a6e9caafcbbf9f
nlloydw <nlloydw61@gmail.co.uk>
2023-03-02 17:02:26 +0000

    2022 to 2023

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -56,7 +56,7 @@
                 <hr class="my-6 border-gray-200 sm:mx-auto lg:my-8">
             @endif
             <div class="flex flex-col sm:flex-row items-center sm:justify-between">
-        <span class="text-sm text-gray-500 text-center">© 2022 <a href="#" class="hover:text-primary">{{ $company_details['company_name'] }}™</a>. All Rights Reserved.
+        <span class="text-sm text-gray-500 text-center">© 2023 <a href="#" class="hover:text-primary">{{ $company_details['company_name'] }}™</a>. All Rights Reserved.
         </span>
                 <div class="flex mt-4 space-x-6 justify-center sm:mt-0">
                     <a href="{{ $company_details['facebook'] }}" class="text-gray-500 hover:text-gray-900">
