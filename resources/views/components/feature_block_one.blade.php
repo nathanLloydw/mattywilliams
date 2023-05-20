1ff7213b1ab3fd969239b5ec973df4f1f58a5f56
nlloydw <nlloydw61@gmail.co.uk>
2023-05-20 20:06:26 +0100

    updated the components to allow for iframes as assets

--- resources/views/components/feature_block_one.blade.php
+++ resources/views/components/feature_block_one.blade.php
@@ -1,11 +1,8 @@
-<section class="container mx-auto px-6 lg:px-14 py-12 lg:flex items-center justify-center lg:space-x-12 2xl:space-x-6">
+<section class="container mx-auto py-6 px-6 lg:px-14 lg:flex items-center">
 
-    <div>
+    <div class="w-full lg:w-1/2">
         {!! $component['header'] !!}
         <p class="mt-4 xl:w-7/12 w-full m-auto">{{ $component['context'] }}</p>
-        <div class="lg:hidden lg:w-3/5 w-full lg:mt-0 mt-6">
-            <img src="{{ $component['image']['permalink'] }}" class="w-full obejct-fit object-center object-fill h-full" />
-        </div>
         <div class="mt-6 md:mt-10 grid sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 gap-6 lg:mt-6 2xl:mt-12">
 
             @foreach($component['features'] as $feature)
@@ -28,8 +25,16 @@
         </div>
     </div>
 
-    <div class="hidden lg:block lg:w-3/5 xl:w-3/5 w-full lg:mt-0 mt-6">
-        <img src="https://i.ibb.co/SKLJ7WX/austin-distel-jp-Hw8ndw-J-Q-unsplash-1.png" alt="ongoing meeting" class="w-full obejct-fit object-center object-fill h-full" />
+    <div class="w-full lg:w-1/2 mt-6 lg:mt-0">
+
+        @if($component['asset_type'] == 'image' && isset($component['image']))
+            <img src="https://i.ibb.co/SKLJ7WX/austin-distel-jp-Hw8ndw-J-Q-unsplash-1.png" alt="ongoing meeting" class="w-full obejct-fit object-center object-fill h-full" />
+        @elseif($component['asset_type'] == 'video' && isset($component['iframe']))
+
+            <div @class(["w-full rounded-lg overflow-hidden"])>{!! $component['iframe'] !!}</div>
+
+        @endif
+
     </div>
 
 </section>
--- resources/views/components/video_block.blade.php
+++ resources/views/components/video_block.blade.php
@@ -1,5 +1,3 @@
 <section class="container py-4 mx-auto px-6 lg:px-14">
-    <div class="m-auto relative pt-[56.25%]">
-        {!! $component['iframe'] !!}
-    </div>
+    {!! $component['iframe'] !!}
 </section>
